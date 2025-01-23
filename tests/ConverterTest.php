4f04ee2cdb75e34affe08c28d10186e52927a74e
Tom <tom@vespertool.com>
2025-01-23 16:37:51 +0100

    Remove parser from ConverterTest

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -2,7 +2,6 @@
 
 use Conversion\Converter;
 use Conversion\Dimension;
-use Conversion\Parser;
 use Conversion\Registry;
 use Conversion\RegistryBuilder;
 use Conversion\Unit;
@@ -12,7 +11,6 @@ use PHPUnit\Framework\TestCase;
 final class ConverterTest extends TestCase
 {
     protected Registry $registry;
-    protected Parser $parser;
     protected Converter $converter;
 
     protected function setUp(): void
@@ -20,14 +18,18 @@ final class ConverterTest extends TestCase
         parent::setUp();
 
         $this->registry = RegistryBuilder::build(new Registry());
-        $this->parser = new Parser($this->registry);
         $this->converter = new Converter($this->registry);
     }
 
     public function test_converts_incompatible_throws()
     {
-        $m = $this->parser->parse('meter');
-        $m2 = $this->parser->parse('meter^2');
+        $m = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 1),
+        );
+        $m2 = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 1),
+            new UnitPart(1, Dimension::LENGTH, 1),
+        );
 
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Cannot convert from [$m] to [$m2]");
@@ -37,21 +39,31 @@ final class ConverterTest extends TestCase
 
     public function test_simple_convert()
     {
-        $km = $this->parser->parse('kilometer');
-        $meter = $this->parser->parse('meter');
-        $hm = $this->parser->parse('hectometer');
+        $km = new Unit(
+            new UnitPart(1000, Dimension::LENGTH, 1),
+        );
+        $m = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 1),
+        );
+        $hm = new Unit(
+            new UnitPart(100, Dimension::LENGTH, 1),
+        );
 
-        $this->assertEquals(2000, $this->converter->convert($km, $meter, 2));
-        $this->assertEquals(0.002, $this->converter->convert($meter, $km, 2));
+        $this->assertEquals(2000, $this->converter->convert($km, $m, 2));
+        $this->assertEquals(0.002, $this->converter->convert($m, $km, 2));
 
-        $this->assertEquals(200, $this->converter->convert($hm, $meter, 2));
+        $this->assertEquals(200, $this->converter->convert($hm, $m, 2));
         $this->assertEquals(0.2, $this->converter->convert($hm, $km, 2));
     }
 
     public function test_area_convert()
     {
-        $km2 = $this->parser->parse('kilometer^2');
-        $m2 = $this->parser->parse('meter^2');
+        $km2 = new Unit(
+            new UnitPart(1000, Dimension::LENGTH, 2),
+        );
+        $m2 = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 2),
+        );
 
         $this->assertEquals(2, $this->converter->convert($m2, $km2, 2_000_000));
         $this->assertEquals(2_000_000, $this->converter->convert($km2, $m2, 2));
@@ -59,8 +71,12 @@ final class ConverterTest extends TestCase
 
     public function test_convert_flattens_dimensions()
     {
-        $km2 = $this->parser->parse('kilometer^2');
-        $m2 = $this->parser->parse('meter^2');
+        $km2 = new Unit(
+            new UnitPart(1000, Dimension::LENGTH, 2),
+        );
+        $m2 = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 2),
+        );
         $meterMeter = new Unit(
             new UnitPart(1, Dimension::LENGTH, 1),
             new UnitPart(1, Dimension::LENGTH, 1),
@@ -71,11 +87,17 @@ final class ConverterTest extends TestCase
 
     public function test_time_convert()
     {
-        $hour = $this->parser->parse('hour');
-        $minute = $this->parser->parse('minute');
-        $second = $this->parser->parse('second');
+        $hour = new Unit(
+            new UnitPart(3600, Dimension::TIME, 1),
+        );
+        $minute = new Unit(
+            new UnitPart(60, Dimension::TIME, 1),
+        );
+        $second = new Unit(
+            new UnitPart(1, Dimension::TIME, 1),
+        );
 
-        $this->assertEquals(120, $this->converter->convert($hour, $minute,2));
+        $this->assertEquals(120, $this->converter->convert($hour, $minute, 2));
         $this->assertEquals(2, $this->converter->convert($minute, $hour, 120));
         $this->assertEquals(120, $this->converter->convert($minute, $second, 2));
         $this->assertEquals(7200, $this->converter->convert($hour, $second, 2));
@@ -83,23 +105,25 @@ final class ConverterTest extends TestCase
 
     public function test_volume_convert()
     {
-        $liter = $this->parser->parse('liter');
-        $dm3 = $this->parser->parse('decimeter^3');
-        $m3 = $this->parser->parse('meter^3');
+        $dm3 = new Unit(
+            new UnitPart(0.1, Dimension::LENGTH, 3),
+        );
+        $m3 = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 3),
+        );
 
-        $this->assertEqualsWithDelta(2, $this->converter->convert($dm3, $liter, 2), 0.000001);
-        $this->assertEqualsWithDelta(2000, $this->converter->convert($m3, $liter, 2), 0.000001);
+        $this->assertEqualsWithDelta(2000, $this->converter->convert($m3, $dm3, 2), 0.000001);
     }
 
     public function test_compound_convert()
     {
         $meterPerSecond = new Unit(
-            new UnitPart( 1, Dimension::LENGTH, 1),
-            new UnitPart( 1, Dimension::TIME, -1),
+            new UnitPart(1, Dimension::LENGTH, 1),
+            new UnitPart(1, Dimension::TIME, -1),
         );
         $kmPerHour = new Unit(
-            new UnitPart( 1000, Dimension::LENGTH, 1),
-            new UnitPart( 3600, Dimension::TIME, -1),
+            new UnitPart(1000, Dimension::LENGTH, 1),
+            new UnitPart(3600, Dimension::TIME, -1),
         );
 
         $this->assertEqualsWithDelta(10, $this->converter->convert($kmPerHour, $meterPerSecond, 36), 0.000001);
@@ -108,9 +132,15 @@ final class ConverterTest extends TestCase
 
     public function test_with_ratios()
     {
-        $kg = $this->parser->parse('kilogram');
-        $m3 = $this->parser->parse('meter^3');
-        $liter = $this->parser->parse('liter');
+        $kg = new Unit(
+            new UnitPart(1, Dimension::MASS, 1),
+        );
+        $m3 = new Unit(
+            new UnitPart(1, Dimension::LENGTH, 3),
+        );
+        $liter = new Unit(
+            new UnitPart(0.1, Dimension::LENGTH, 3),
+        );
 
         $converterWithDensity = $this->converter->withRatios(["$kg/$m3" => 1000]);
 
