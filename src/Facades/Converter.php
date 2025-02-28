837d028698acf95aa0392e780966cc758496f440
vovabndrnk <volodymyr@vespertool.com>
2025-02-28 10:51:18 +0100

    Return Parser and Registry

--- src/Facades/Converter.php
+++ src/Facades/Converter.php
@@ -3,11 +3,12 @@
 namespace Conversion\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Conversion\Converter as ConversionConverter;
 
 class Converter extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'converter';
+        return ConversionConverter::class;
     }
 }
--- src/Providers/ConversionServiceProvider.php
+++ src/Providers/ConversionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Conversion\Providers;
 
+use Conversion\Parser;
+use Conversion\Registry;
 use Illuminate\Support\ServiceProvider;
 use Conversion\Converter;
 
@@ -11,6 +13,16 @@ class ConversionServiceProvider extends ServiceProvider
     {
         $this->app->singleton(Converter::class, function ($app) {
             return new Converter();
+        });
+
+        $this->app->singleton(Parser::class, function ($app) {
+            $registry = $app->make('registry');
+
+            return new Parser($registry);
+        });
+
+        $this->app->singleton(Registry::class, function ($app) {
+            return new Registry();
         });
     }
 }
